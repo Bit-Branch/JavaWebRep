02e501339f4ecd87c55dada42bc05970079c5d50
Bit-Branch <55889623+Bit-Branch@users.noreply.github.com>
2020-07-16 12:20:05 +0300

    Update MySpecialties.java

--- epam committiee/src/main/java/by/epam/committiee/command/impl/MySpecialties.java
+++ epam committiee/src/main/java/by/epam/committiee/command/impl/MySpecialties.java
@@ -1,4 +1,26 @@
 package by.epam.committiee.command.impl;
 
-public class MySpecialties {
+public class MySpecialties implements ActionCommand{
+    
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String page;
+        HttpSession session = request.getSession();
+
+        ServiceWarehouse serviceWarehouse = ServiceWarehouse.getInstance();
+        SpecialtyService service = serviceWarehouse.getSpecialtyService();
+        
+                List<Specialty> specialties = service.findAll();
+                request.setAttribute("specialties", specialities);
+               
+             
+                page = PathPage.HOME;
+            }catch (ServiceException e){
+                page = PathPage.LOGIN;
+            }
+        } else {
+            page = PathPage.LOGIN;
+        }
+        return page;
+    }
 }
